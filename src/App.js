7fb7015d44b0ca902b847dbd69bb83f8ad796627
Viktor Kravchuk <viktor.kravchuk@buffl.be>
2020-10-10 23:22:39 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import MainPage from './components/MainPage';
+import MusicPage from './components/MusicPage';
+import ContactPage from './components/ContactPage';
+import SocialPage from './components/SocialPage';
 
-export default App;
+const pages = {
+  MAIN: 'MAIN',
+  MUSIC: 'MUSIC',
+  CONTACT: 'CONTACT',
+  SOCIAL: 'SOCIAL',
+};
+
+export default () => {
+  const [currentPage, setCurrentPage] = useState(pages.MAIN);
+
+  switch (currentPage) {
+    case 'MAIN':
+      return <MainPage />;
+    case 'MUSIC':
+      return <MusicPage />;
+    case 'CONTACT':
+      return <ContactPage />;
+    case 'SOCIAL':
+      return <SocialPage />;
+  }
+};
---
+++ src/components/MusicPage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <h1>MUSIC PAGE</h1>;
+};
